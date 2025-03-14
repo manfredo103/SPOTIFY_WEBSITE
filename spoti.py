a761365b9d37fb3d23c10f5d5078250ac3267f58
Markus Li <markus.li@fau.de>
2025-03-14 18:17:26 +0100

    fixed single acc issue

--- spoti.py
+++ spoti.py
@@ -1,6 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask, redirect, session, request, render_template
+from flask_session import Session
 import time
 import json 
 from dotenv import load_dotenv
@@ -10,7 +11,14 @@ load_dotenv()
 # Flask app setup
 app = Flask(__name__)
 app.secret_key = os.getenv("secret_key")
-app.config['SESSION_COOKIE_NAME'] = 'spotify-auth-session'
+app.config["SESSION_TYPE"] = "filesystem"  # Stores sessions on the server
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_USE_SIGNER"] = True
+app.config["SESSION_KEY_PREFIX"] = "spotify_"  # Prevents key conflicts
+app.config["SESSION_FILE_DIR"] = os.path.abspath("/home/manfred/Documents/SPOTIFY_WEBSITE/flask_sessions")  # Where session files are stored
+Session(app)
+
+
 
 # Set your Spotify credentials and redirect URI
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
@@ -27,23 +35,26 @@ sp_oauth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIEN
 #@app.route('/xd')
 def expired():
     #print("Inside expired function")
-    token_info = session.get('token_info', None)
     
+    user_id = session.get("spotify_username")
+    token_info = session.get(f"token_info_{user_id}")
+    print(token_info)
+
     if not token_info:
-        return redirect('/login')
+        return None
     
     #print("shdd")
 
-    print(token_info['expires_at'])
-    print(int(time.time()))
+    #print(token_info['expires_at'])
+    #print(int(time.time()))
     
-    if token_info['expires_at'] - time.time() < 60:
-        print("access token expired... requesting new one")
-        token_info = sp_oauth.refresh_access_token(refresh_token=token_info['refresh_token'])
-        session['token_info'] = token_info
+    # if token_info['expires_at'] - time.time() < 60:
+    #     print("access token expired... requesting new one")
+    #     token_info = sp_oauth.refresh_access_token(refresh_token=token_info['refresh_token'])
+    #     session['token_info'] = token_info
+    token = sp_oauth.validate_token(token_info)
 
-    #print("xdd")
-    return token_info['access_token']
+    return spotipy.Spotify(auth=token_info)
 
 def save_json_to_file(data, filename='user_activity.json'):
     with open(filename, 'w') as f:
@@ -52,6 +63,7 @@ def save_json_to_file(data, filename='user_activity.json'):
 
 @app.route('/')
 def home():
+    #print(f"something{app.config['SESSION_FILE_DIR']}")
     return render_template('index.html')
 
 
@@ -63,17 +75,26 @@ def login():
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    sp = spotipy.Spotify(auth=token_info["access_token"])
+    token_info = sp_oauth.get_access_token(code, as_dict=False,check_cache=False)
+
+    
+
+    sp = spotipy.Spotify(auth=token_info)
     user_info = sp.current_user()
-    session['spotify_username'] = user_info["id"]
-    session["token_info"] = token_info
+    print(token_info)
+    print(user_info)
+    user_id = user_info["id"]
+    
+    session['spotify_username'] = user_id
+    session[f"token_info_{user_id}"] = token_info
+    
     return redirect('/')
 
 @app.route('/playlists')
 def get_playlists():
-    token_info = expired()
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
+    if not sp:
+        return redirect('/login')
     playlists = sp.current_user_playlists()
     save_json_to_file(playlists,"playlists.json")
     pl = playlists['items']
@@ -82,9 +103,9 @@ def get_playlists():
 
 @app.route('/get_activity')
 def get_activity():
-    token_info = expired()
-    
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
+    if not sp:
+        return redirect('/login')
     useractivity = sp.current_user_playing_track()
     save_json_to_file(useractivity, 'user_activity.json')
     if useractivity and useractivity.get('item'):
@@ -97,44 +118,43 @@ def get_activity():
 
 @app.route('/activity')
 def currently_playing(): 
-    expired()
+    sp = expired()
+    if not sp:
+        return redirect('/login')
     return render_template('currentlyplaying.html')
 
 @app.route('/skip')
 def skip_track():
-    token_info = expired()
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
     sp.next_track()
     
-
     return '', 204
 
 @app.route('/back')
 def back():
-    token_info = expired()
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
     sp.previous_track()
     
     return '', 204
 
 @app.route('/top_songs')
 def top_songs(time_range="medium_term"):
-    #print("should print")
-    token_info = expired()
-    #print("if i see this i am mad")
-   
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
+    if not sp:
+        return redirect('/login')
+    user = sp.current_user()
     josn = sp.current_user_top_tracks(time_range='short_term')
     save_json_to_file(josn, "toptracks.json")
     songs = josn['items']
     ##exit()
 
-    return render_template('Top_Songs.html', songs = songs)
+    return render_template('Top_Songs.html', songs = songs, user = user)
 
 @app.route('/top_artists')
 def top_artists():
-    token_info = expired()
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
+    if not sp:
+        return redirect('/login')
     josn = sp.current_user_top_artists(time_range='short_term')
     save_json_to_file(josn, "toptracks.json")
 
@@ -143,8 +163,9 @@ def top_artists():
 
 @app.route('/genre')
 def genre():
-    token_info = expired()
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
+    if not sp:
+        return redirect('/login')
     josn = sp.current_user_top_artists(time_range='short_term')
     genre = josn['items']
     save_json_to_file(genre, "genres.json")
@@ -156,8 +177,9 @@ def queue():
 
 @app.route('/songsearch', methods=['GET'])
 def searchsong():
-    token_info = expired()
-    sp = spotipy.Spotify(auth=token_info)
+    sp = expired()
+    if not sp:
+        return redirect('/login')
     lookingfor = request.args.get('song')
     #print(lookingfor)
     josn = sp.search(q=lookingfor)
@@ -170,10 +192,11 @@ def searchsong():
     
 @app.route('/songqueue', methods=['GET'])
 def songqueue():
-    token_info = expired()
+    sp = expired()
+    if not sp:
+        return redirect('/login')
     id = request.args.get('id')
     print(id)
-    sp = spotipy.Spotify(auth=token_info)
     response = sp.add_to_queue(uri=id)
 
     if response:
